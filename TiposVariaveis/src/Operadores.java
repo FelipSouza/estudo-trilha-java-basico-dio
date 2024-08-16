6675435d05206542e2f6f756cb4891c1acab0f87
FelipSouza <felipe.souza.facco@gmail.com>
2024-08-15 20:42:37 -0500

    implementando classe de operadores para estudar os operadores do java

---
+++ TiposVariaveis/src/Operadores.java
@@ -0,0 +1,23 @@
+public class Operadores {
+    public static void main(String[] args) {
+        
+        int a, b;
+
+        a = 2;
+        b = 5;
+        /* 
+        if (a>b)
+            System.out.println("a é maior que b");
+        else 
+            System.out.println("a não é maior que b");
+        */
+        
+
+        //operador ternário
+        String resultado = a>b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
+        
+
+    }
+}
